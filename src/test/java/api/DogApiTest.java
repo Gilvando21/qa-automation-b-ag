a6753359d7b4f2c9a6c7694f3d3208aef0d954ad
GilvandoSenaMatos <gilvando0321@gmail.com>
2026-03-24 12:41:19 -0300

    ci: trigger pipeline

--- src/test/java/api/DogApiTest.java
+++ src/test/java/api/DogApiTest.java
@@ -1,15 +1,69 @@
-
 package api;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class DogApiTest {
 
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "https://dog.ceo/api";
+    }
+
     @Test
-    public void testListBreeds(){
-        var res = RestAssured.get("https://dog.ceo/api/breeds/list/all");
-        assertEquals(200, res.statusCode());
+    public void deveListarRacas() {
+
+        Response response = RestAssured
+                .given()
+                .log().all()
+                .when()
+                .get("/breeds/list/all");
+
+        // Status code
+        Assert.assertEquals(200, response.getStatusCode());
+
+        // Conteúdo da resposta
+        Assert.assertTrue(response.jsonPath().getMap("message").size() > 0);
+
+        // Diferencial Sênior (performance)
+        Assert.assertTrue("Tempo de resposta acima do esperado",
+                response.getTime() < 2000);
+    }
+
+    @Test
+    public void deveRetornarImagemAleatoria() {
+
+        Response response = RestAssured
+                .given()
+                .log().all()
+                .when()
+                .get("/breeds/image/random");
+
+        // Status code
+        Assert.assertEquals(200, response.getStatusCode());
+
+        // Valida URL da imagem
+        String imageUrl = response.jsonPath().getString("message");
+        Assert.assertTrue(imageUrl.contains("http"));
+
+        // Performance
+        Assert.assertTrue("Tempo de resposta acima do esperado",
+                response.getTime() < 2000);
+    }
+
+    @Test
+    public void naoDeveEncontrarRacaInvalida() {
+
+        Response response = RestAssured
+                .given()
+                .log().all()
+                .when()
+                .get("/breed/racaInvalidaXYZ/images");
+
+        // Status esperado (erro)
+        Assert.assertEquals(404, response.getStatusCode());
     }
 }
